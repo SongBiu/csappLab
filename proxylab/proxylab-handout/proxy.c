fa25665328214ca938ace11cd6abf5aec2d1a3bd
Song Li <v-soli@microsoft.com>
2018-09-16 17:22:53 +0800

    parse_uri

--- proxylab/proxylab-handout/proxy.c
+++ proxylab/proxylab-handout/proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "csapp.h"
 /* Recommended max cache and object sizes */
@@ -7,40 +8,42 @@
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+void parse_uri(char *uri, char *hostname, char *port, char *path);
 void doit(int fd);
 void build_http_header(char* method, char *uri, char* version, char* buf);
-void parse_uri(char* uri, char* hostname, char* port, char* path);
+
 int main(int argc, char **argv)
 {
-    // int listenfd, connfd;
-    // char hostname[MAXLINE], port[MAXLINE];
-    // socklen_t clientlen;
-    // struct sockaddr_storage clientaddr;
-    // if (argc != 2) {
-    //     fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    //     exit(1);
-    // }
-    // listenfd = Open_listenfd(argv[1]);
-    // while (1)
-    // {
-    //     clientlen = sizeof(clientaddr);
-	//     connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-    //     Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
-    //                 port, MAXLINE, 0);
-    //     printf("Accepted connection from (%s, %s)\n", hostname, port);
-    //     doit(connfd);
-    // }
-    // printf("%s", user_agent_hdr);
-    // return 0;
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    listenfd = Open_listenfd(argv[1]);
+    while (1)
+    {
+        clientlen = sizeof(clientaddr);
+	    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
+                    port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        doit(connfd);
+    }
+    printf("%s", user_agent_hdr);
+    return 0;
 }
+
 void doit(int fd) 
 {
     int clientfd;
-    int port;
     struct hostent *host_server;
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-    char hostname[MAXLINE], path[MAXLIne];
+    char hostname[MAXLINE], path[MAXLINE], port[MAXLINE];
     rio_t rio_server, rio_client;
+    struct in_addr ina;
 
     Rio_readinitb(&rio_server, fd);
     if (!Rio_readlineb(&rio_server, buf, MAXLINE))
@@ -48,27 +51,24 @@ void doit(int fd)
         return;
     } 
         
-    printf("%s", buf);
     sscanf(buf, "%s %s %s", method, uri, version);
-    parse_uri(uri, hostname, &port, path);
-    // printf("method:%s\nuri:%s\nversion:%s\n", method, uri, version);
-    // host_server = Gethostbyname(uri);
-    // Connect(clientfd, (struct sockaddr*)host_server->h_addr, sizeof((struct sockaddr*)host_server->h_addr));
-    // Rio_writen(clientfd, )
+    parse_uri(uri, hostname, port, path);
+    printf("%s\n%s\n", hostname, port);
+    clientfd = Open_clientfd(hostname, port);
+
     return;
 }
-void parse_uri(char *uri, char *hostname, int *port, char *path)
+void parse_uri(char *uri, char *hostname, char *port, char *path)
 {
-    *port = 80;
-    int i;
+    int p = 80;
     char *pos1 = strstr(uri, "//"), *pos2=NULL;
     pos1 = pos1?pos1 + 2: uri;
     pos2 = strstr(pos1, ":");
     if (pos2)
     {
-        *pos2 = '\n';
+        *pos2 = '\0';
         sscanf(pos1, "%s", hostname);
-        sscanf(pos2 + 1, "%d%s", port, path);
+        sscanf(pos2 + 1, "%d%s", &p, path);
     }
     else
     {
@@ -78,6 +78,10 @@ void parse_uri(char *uri, char *hostname, int *port, char *path)
         *pos2 = '/';
         sscanf(pos2, "%s", path);
     }
-    return ;
+    sprintf(port, "%d", p);
+    return;
+}
+void build_http_header(char *method, char *uri, char *version, char *header)
+{
 
 }
